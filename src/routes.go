aa8cbb716200c7a05370b235d4322e86eb1c1fc5
hsgilliland <jo-ash@procentrica.co.za>
2020-06-01 09:55:23 +0200

    changed to get

--- src/routes.go
+++ src/routes.go
@@ -5,5 +5,5 @@ func (s *Server) routes() {
 	s.router.HandleFunc("/user", s.handleupdateuser()).Methods("PUT")
 	s.router.HandleFunc("/login", s.handleloginuser()).Methods("POST")
 	s.router.HandleFunc("/userpassword", s.handlechangeuserpassword()).Methods("PUT")
-	s.router.HandleFunc("/forgotpassword", s.handleforgotpassword()).Methods("POST")
+	s.router.HandleFunc("/forgotpassword", s.handleforgotpassword()).Methods("GET")
 }
